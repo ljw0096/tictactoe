867b302aa559d81ef501e8c83eb0ba7892380f8d
ljw0096 <ljw0096@naver.com>
2022-10-09 16:24:27 +0900

    feat: add time travel

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,6 +9,7 @@ const Game = () => {
         squares: Array(9).fill(null),
       },
     ],
+    stepNumber: 0,
     xIsNext: true,
   })
   const current = state.history[state.history.length - 1]
@@ -16,22 +17,44 @@ const Game = () => {
   let status = winner ? `Winner: ${winner}` : `Next Player: ${state.xIsNext ? "X" : "O"}`
 
   const handleClick = (i: number) => {
-    const history = state.history
+    const history = state.history.slice(0, state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) return
     squares[i] = state.xIsNext ? "X" : "O"
-    setState({ ...state, history: history.concat([{ squares: squares }]), xIsNext: !state.xIsNext })
+    setState({
+      ...state,
+      history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
+      xIsNext: !state.xIsNext,
+    })
+  }
+
+  const jumpTo = (step: number) => {
+    setState({
+      ...state,
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    })
   }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={handleClick} />
+        <Board squares={state.history[state.stepNumber].squares} onClick={handleClick} />
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* todo */}</ol>
+        <ol>
+          {state.history.map((step, move) => {
+            const desc = move ? `Go to move #${move}` : `Go to game start`
+            return (
+              <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+              </li>
+            )
+          })}
+        </ol>
       </div>
     </div>
   )
